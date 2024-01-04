e18c8a23ef3a3dab87e406ed4e2259e36d6305d9
Shivam Patel <shivam.patel@tatatechnologies.com>
2024-01-04 10:34:45 +0530

    project structure and implementation

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const { rateLimit } = require('express-rate-limit')
+
+const rateLimitMiddleware = rateLimit({
+	windowMs: 1 * 60 * 1000, // 15 minutes
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', rateLimitMiddleware);
+app.use('/', rateLimitMiddleware, indexRouter);
+app.use('/users', rateLimitMiddleware, usersRouter);
+app.get('/okay', (req, res) => {
+  res.send({msg: 'Hello'})
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
